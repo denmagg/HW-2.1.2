06e74497967e7ad78d60c9b88c1d87d5ceaac2ae
Denis <denmagg.work@gmail.com>
2021-06-26 05:55:25 +0300

    Initial

--- HW 2.1.2/ViewController.swift
+++ HW 2.1.2/ViewController.swift
@@ -10,11 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //    MARK: @IBOutlets
+
+    @IBOutlet var button: UIButton!
+    
+    @IBOutlet var trafficLights: [UIView]!
+    
+    
+    enum CurrentLight {
+        case red
+        case yellow
+        case green
+    }
+    var currentLight: CurrentLight = .green
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        for trafficLight in trafficLights {
+            trafficLight.layer.cornerRadius = trafficLight.frame.size.height / 2
+        }
     }
-
-
+    
+    @IBAction func buttonAction(){
+        button.setTitle("NEXT", for: .normal)
+        trafficLights[0].alpha = 0.3
+        trafficLights[1].alpha = 0.3
+        trafficLights[2].alpha = 0.3
+        switch currentLight {
+        case .green:
+            trafficLights[0].alpha = 1
+            trafficLights[2].alpha = 0.3
+            currentLight = .red
+        case .red:
+            trafficLights[0].alpha = 0.3
+            trafficLights[1].alpha = 1
+            currentLight = .yellow
+        case .yellow:
+            trafficLights[1].alpha = 0.3
+            trafficLights[2].alpha = 1
+            currentLight = .green
+        }
+    }
 }
 
